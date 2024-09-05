aac301b99bbadddde247f5e17345fe1903ab62ac
Max Romashoff <m4xromashoff@yandex.ru>
2024-09-05 16:06:03 +0300

    -up to 12

---
+++ inserts.sql
@@ -0,0 +1,35 @@
+USE gb_final_task;
+INSERT INTO cat (cat_name, date_birth, commands, color)
+VALUES
+( 'cat1', '2024-01-01', 'go', 'black0'),
+( 'cat2', '2024-02-01', 'go', 'black1'),
+( 'cat3', '2024-03-01', 'go', 'black2'),
+( 'cat4', '2024-04-01', 'go', 'black3');
+
+INSERT INTO dog (dog_name, date_birth, commands, color)
+VALUES
+( 'dog1', '2024-01-01', 'go', 'black0'),
+( 'dog2', '2024-02-01', 'go', 'black1'),
+( 'dog3', '2024-03-01', 'go', 'black2'),
+( 'dog4', '2024-04-01', 'go', 'black3');
+
+INSERT INTO hamster (hamster_name, date_birth, commands, color)
+VALUES
+( 'ham1', '2024-01-01', 'go', 'black0'),
+( 'ham2', '2024-02-01', 'go', 'black1'),
+( 'ham3', '2024-03-01', 'go', 'black2'),
+( 'ham4', '2024-04-01', 'go', 'black3');
+
+INSERT INTO horse (name, date_birth, commands, color)
+VALUES
+( 'horse1', '2024-01-01', 'go', 'black0'),
+( 'horse2', '2024-02-01', 'go', 'black1'),
+( 'horse3', '2024-03-01', 'go', 'black2'),
+( 'horse4', '2024-04-01', 'go', 'black3');
+
+INSERT INTO mule (name, date_birth, commands, color)
+VALUES
+( 'mu1', '2024-01-01', 'go', 'black0'),
+( 'mue2', '2024-02-01', 'go', 'black1'),
+( 'mu3', '2024-03-01', 'go', 'black2'),
+( 'mu4', '2024-04-01', 'go', 'black3');
